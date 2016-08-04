7b6c69bd0ccfa1f7fe19d5eabf7e7bf881be8dcd
Henrique Gouveia <henriquec@ciandt.com>
2016-08-04 09:05:53 -0300

    Handling Intents

--- SiriKitExampleIntent/IntentHandler.swift
+++ SiriKitExampleIntent/IntentHandler.swift
@@ -17,7 +17,7 @@ import Intents
 // "<myApp> John saying hello"
 // "Search for messages in <myApp>"
 
-class IntentHandler: INExtension, INSendMessageIntentHandling, INSearchForMessagesIntentHandling, INSetMessageAttributeIntentHandling {
+class IntentHandler: INExtension, INSendMessageIntentHandling {
     
     override func handler(for intent: INIntent) -> AnyObject {
         // This is the default implementation.  If you want different objects to handle different intents,
@@ -31,13 +31,13 @@ class IntentHandler: INExtension, INSendMessageIntentHandling, INSearchForMessag
     // Implement resolution methods to provide additional information about your intent (optional).
     func resolveRecipients(forSendMessage intent: INSendMessageIntent, with completion: ([INPersonResolutionResult]) -> Void) {
         if let recipients = intent.recipients {
-            
+
             // If no recipients were provided we'll need to prompt for a value.
             if recipients.count == 0 {
                 completion([INPersonResolutionResult.needsValue()])
                 return
             }
-            
+
             var resolutionResults = [INPersonResolutionResult]()
             for recipient in recipients {
                 let matchingContacts = [recipient] // Implement your contact matching logic here to create an array of matching contacts
@@ -45,27 +45,38 @@ class IntentHandler: INExtension, INSendMessageIntentHandling, INSearchForMessag
                 case 2  ... Int.max:
                     // We need Siri's help to ask user to pick one from the matches.
                     resolutionResults += [INPersonResolutionResult.disambiguation(with: matchingContacts)]
-                    
+
                 case 1:
                     // We have exactly one matching contact
                     resolutionResults += [INPersonResolutionResult.success(with: recipient)]
-                    
+
                 case 0:
                     // We have no contacts matching the description provided
                     resolutionResults += [INPersonResolutionResult.unsupported()]
-                    
+
                 default:
                     break
-                    
+
                 }
             }
             completion(resolutionResults)
+            
         }
     }
-    
+
     func resolveContent(forSendMessage intent: INSendMessageIntent, with completion: (INStringResolutionResult) -> Void) {
         if let text = intent.content, !text.isEmpty {
-            completion(INStringResolutionResult.success(with: text))
+            let contents = DataProvider.filterItem(itemDescription: text)
+            switch contents.count {
+            case 0:
+                completion(INStringResolutionResult.needsValue())
+                break
+            case 1:
+                completion(INStringResolutionResult.success(with: text))
+                break
+            default:
+                completion(INStringResolutionResult.disambiguation(with: contents.map { "\($0.category) \($0.model) \($0.color)" }))
+            }
         } else {
             completion(INStringResolutionResult.needsValue())
         }
@@ -91,26 +102,7 @@ class IntentHandler: INExtension, INSendMessageIntentHandling, INSearchForMessag
         completion(response)
     }
     
-    // Implement handlers for each intent you wish to handle.  As an example for messages, you may wish to also handle searchForMessages and setMessageAttributes.
-    
-    // MARK: - INSearchForMessagesIntentHandling
-    
-    func handle(searchForMessages intent: INSearchForMessagesIntent, completion: (INSearchForMessagesIntentResponse) -> Void) {
-        // Implement your application logic to find a message that matches the information in the intent.
-        
-        let userActivity = NSUserActivity(activityType: NSStringFromClass(INSearchForMessagesIntent.self))
-        let response = INSearchForMessagesIntentResponse(code: .success, userActivity: userActivity)
-        // Initialize with found message's attributes
-        response.messages = [INMessage(
-            identifier: "identifier",
-            content: "I am so excited about SiriKit!",
-            dateSent: Date(),
-            sender: INPerson(personHandle: INPersonHandle(value: "sarah@example.com", type: .emailAddress), nameComponents: nil, displayName: "Sarah", image: nil,  contactIdentifier: nil, customIdentifier: nil),
-            recipients: [INPerson(personHandle: INPersonHandle(value: "+1-415-555-5555", type: .phoneNumber), nameComponents: nil, displayName: "John", image: nil,  contactIdentifier: nil, customIdentifier: nil)]
-            )]
-        completion(response)
-    }
-    
+
     // MARK: - INSetMessageAttributeIntentHandling
     
     func handle(setMessageAttribute intent: INSetMessageAttributeIntent, completion: (INSetMessageAttributeIntentResponse) -> Void) {
--- SiriKitExampleIntentUI/IntentViewController.swift
+++ SiriKitExampleIntentUI/IntentViewController.swift
@@ -16,28 +16,58 @@ import IntentsUI
 // "Send a message using <myApp>"
 
 class IntentViewController: UIViewController, INUIHostedViewControlling {
-    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemDescriptionLabel: UILabel!
+    @IBOutlet weak var itemPriceLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    // MARK: Get Model from Data Provider
+
+    private func fetchMessageIntent(string: String?) -> Item? {
+        if let string = string {
+            let fetchedResult = DataProvider.filterItem(itemDescription: string)
+            if fetchedResult.count > 0 {
+                if let item = fetchedResult.first {
+                    return item
+                }
+            }
+        }
+
+        return nil
+    }
+
+    private func fillFields(item: Item) {
+        self.itemImageView.image = UIImage(named: item.imageName)
+        self.itemDescriptionLabel.text = "\(item.category) \(item.model) \(item.color)"
+        self.itemPriceLabel.text = "R$ \(item.price)"
+    }
+
     // MARK: - INUIHostedViewControlling
-    
+
     // Prepare your view controller for the interaction to handle.
     func configure(with interaction: INInteraction!, context: INUIHostedViewContext, completion: ((CGSize) -> Void)!) {
         // Do configuration here, including preparing views and calculating a desired size for presentation.
-        
+
+        if let messageIntent = interaction.intent as? INSendMessageIntent {
+            if let item = self.fetchMessageIntent(string: messageIntent.content) {
+                self.fillFields(item: item)
+            }
+        }
+
         if let completion = completion {
             completion(self.desiredSize)
         }
     }
-    
+
     var desiredSize: CGSize {
         return self.extensionContext!.hostedViewMaximumAllowedSize
     }
